c1fb8d65484d3cbe569bc09d509b1fcb34f35bb3
MINAS\User <ics20072@uom.edu.gr>
2024-06-20 22:29:11 +0300

    Question frame was improved

--- QuestionPopUp.java
+++ QuestionPopUp.java
@@ -10,58 +10,15 @@ public class QuestionPopUp extends Actor
     
     public QuestionPopUp(Question question) 
     {
-        makeImage("Question", question.getQuestion(), question.getOptions());
         this.correctAnswer = showQuestionPopup(question.getQuestion(), question.getOptions(), question.getCorrectOption());
-    }
-    
-    private void makeImage(String title, String questionText, String[] options) 
-    {
-        GreenfootImage image = new GreenfootImage(WIDTH, HEIGHT);
-    
-        image.setColor(new Color(255, 255, 255, 128));
-        image.fillRect(0, 0, WIDTH, HEIGHT);
-        image.setColor(new Color(0, 0, 0, 128));
-        image.fillRect(5, 5, WIDTH - 10, HEIGHT - 10);
-    
-        Font font = image.getFont();
-        font = font.deriveFont(FONT_SIZE);
-        image.setFont(font);
-        image.setColor(Color.WHITE);
-        image.drawString(title, 60, 50);
-        image.drawString(questionText, 60, 100);
-    
-        for (int i = 0; i < options.length; i++) 
-        {
-            image.drawString(options[i], 60, 150 + i * 40);
-        }
-    
-        setImage(image);
     }
     
     public boolean showQuestionPopup(String question, String[] options, int correctOption) 
     {
-        GreenfootImage image = new GreenfootImage(WIDTH, HEIGHT);
-    
-        image.setColor(new Color(255, 255, 255, 128));
-        image.fillRect(0, 0, WIDTH, HEIGHT);
-        image.setColor(new Color(0, 0, 0, 128));
-        image.fillRect(5, 5, WIDTH - 10, HEIGHT - 10);
-    
-        Font font = image.getFont();
-        font = font.deriveFont(FONT_SIZE);
-        image.setFont(font);
-        image.setColor(Color.WHITE);
-        image.drawString(question, 60, 100);
-    
-        for (int i = 0; i < options.length; i++) 
-        {
-            image.drawString(options[i], 60, 150 + i * 40);
-        }
-    
-        setImage(image);
-    
-        int answer = Greenfoot.ask(question + "\n" + String.join("\n", options)).charAt(0) - 'a';
-        return answer == correctOption;
+        String answer = Greenfoot.ask(question + "\n" + String.join("\n", options) + "\nEnter your answer (a, b, c, d):");
+        int answerIndex = answer.charAt(0) - 'a';
+        
+        return answerIndex == correctOption;
     }
     
     public boolean isCorrectAnswer() 
